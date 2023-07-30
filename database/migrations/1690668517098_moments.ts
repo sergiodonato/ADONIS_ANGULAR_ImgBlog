175dc79b7319ee4d0888210a9edcdc7f67807492
Sergio Donato <sergiodcdn@gmail.com>
2023-07-30 16:48:08 -0300

    p

--- database/migrations/1690668517098_moments.ts
+++ database/migrations/1690668517098_moments.ts
@@ -3,9 +3,12 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'moments'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('title')
+      table.string('description')
+      table.string('image')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
@@ -15,7 +18,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
